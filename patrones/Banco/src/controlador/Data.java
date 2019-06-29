b23cec5b182faf605ec12a3b40485b07d5dce8e2
Flavio Toro B <38701988+fulkesko@users.noreply.github.com>
2019-06-29 16:45:49 -0400

    asd

--- patrones/Banco/src/controlador/Data.java
+++ patrones/Banco/src/controlador/Data.java
@@ -1,14 +1,11 @@
 package controlador;
 
-import gui.GUI;
 import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Random;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.stream.IntStream;
 import modelo.Tarjeta;
 
