934c7cc4f163cdb103101cdac3bc413b518ee09b
Flavio Toro B <38701988+fulkesko@users.noreply.github.com>
2019-07-01 13:16:33 -0400

    imagen tarjeta

--- patrones/Banco/src/gui/GUI_TarjetaTransferencias.java
+++ patrones/Banco/src/gui/GUI_TarjetaTransferencias.java
@@ -27,40 +27,218 @@ public class GUI_TarjetaTransferencias extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
+        lblb1 = new javax.swing.JLabel();
+        lbla3 = new javax.swing.JLabel();
+        lbla4 = new javax.swing.JLabel();
+        lbla5 = new javax.swing.JLabel();
+        lbla6 = new javax.swing.JLabel();
+        lbla7 = new javax.swing.JLabel();
+        lbla8 = new javax.swing.JLabel();
+        lbla9 = new javax.swing.JLabel();
+        lbla1 = new javax.swing.JLabel();
+        lblb2 = new javax.swing.JLabel();
+        lblb3 = new javax.swing.JLabel();
+        lblb4 = new javax.swing.JLabel();
+        lbl10 = new javax.swing.JLabel();
+        lblb5 = new javax.swing.JLabel();
+        lblb6 = new javax.swing.JLabel();
+        lblb7 = new javax.swing.JLabel();
+        lblb8 = new javax.swing.JLabel();
+        lblb9 = new javax.swing.JLabel();
+        lblb10 = new javax.swing.JLabel();
+        lblc1 = new javax.swing.JLabel();
+        lblc2 = new javax.swing.JLabel();
+        lblc3 = new javax.swing.JLabel();
+        lblc4 = new javax.swing.JLabel();
+        lblc5 = new javax.swing.JLabel();
+        lbld1 = new javax.swing.JLabel();
+        lble1 = new javax.swing.JLabel();
+        lbld2 = new javax.swing.JLabel();
+        lble2 = new javax.swing.JLabel();
+        lbld3 = new javax.swing.JLabel();
+        lble3 = new javax.swing.JLabel();
+        lbld4 = new javax.swing.JLabel();
+        lble4 = new javax.swing.JLabel();
+        lbld5 = new javax.swing.JLabel();
+        lble5 = new javax.swing.JLabel();
+        lblc6 = new javax.swing.JLabel();
+        lbld6 = new javax.swing.JLabel();
+        lble6 = new javax.swing.JLabel();
+        lblc7 = new javax.swing.JLabel();
+        lbld7 = new javax.swing.JLabel();
+        lble7 = new javax.swing.JLabel();
+        lblc8 = new javax.swing.JLabel();
+        lbld8 = new javax.swing.JLabel();
+        lble8 = new javax.swing.JLabel();
+        lblc9 = new javax.swing.JLabel();
+        lbld9 = new javax.swing.JLabel();
+        lble9 = new javax.swing.JLabel();
+        lblc10 = new javax.swing.JLabel();
+        lbld10 = new javax.swing.JLabel();
+        lble10 = new javax.swing.JLabel();
+        lbla2 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setPreferredSize(new java.awt.Dimension(860, 550));
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jPanel1.setPreferredSize(new java.awt.Dimension(858, 544));
+        lblb1.setText("jLabel2");
+        getContentPane().add(lblb1, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 170, 50, 40));
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 858, Short.MAX_VALUE)
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 544, Short.MAX_VALUE)
-        );
+        lbla3.setText("jLabel2");
+        getContentPane().add(lbla3, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 110, 60, 40));
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
+        lbla4.setText("jLabel2");
+        getContentPane().add(lbla4, new org.netbeans.lib.awtextra.AbsoluteConstraints(320, 110, 60, 40));
+
+        lbla5.setText("jLabel2");
+        getContentPane().add(lbla5, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 110, 60, 40));
+
+        lbla6.setText("jLabel2");
+        getContentPane().add(lbla6, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 110, 60, 40));
+
+        lbla7.setText("jLabel2");
+        getContentPane().add(lbla7, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 110, 60, 40));
+
+        lbla8.setText("jLabel2");
+        getContentPane().add(lbla8, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 110, 50, 40));
+
+        lbla9.setText("jLabel2");
+        getContentPane().add(lbla9, new org.netbeans.lib.awtextra.AbsoluteConstraints(670, 110, 60, 40));
+
+        lbla1.setText("jLabel2");
+        getContentPane().add(lbla1, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 120, -1, 30));
+
+        lblb2.setText("jLabel2");
+        getContentPane().add(lblb2, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 170, 60, 40));
+
+        lblb3.setText("jLabel2");
+        getContentPane().add(lblb3, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 170, 60, 40));
+
+        lblb4.setText("jLabel2");
+        getContentPane().add(lblb4, new org.netbeans.lib.awtextra.AbsoluteConstraints(320, 170, 60, 40));
+
+        lbl10.setText("jLabel2");
+        getContentPane().add(lbl10, new org.netbeans.lib.awtextra.AbsoluteConstraints(750, 110, 60, 40));
+
+        lblb5.setText("jLabel2");
+        getContentPane().add(lblb5, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 170, 60, 40));
+
+        lblb6.setText("jLabel2");
+        getContentPane().add(lblb6, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 170, 60, 40));
+
+        lblb7.setText("jLabel2");
+        getContentPane().add(lblb7, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 170, 60, 40));
+
+        lblb8.setText("jLabel2");
+        getContentPane().add(lblb8, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 170, 60, 40));
+
+        lblb9.setText("jLabel2");
+        getContentPane().add(lblb9, new org.netbeans.lib.awtextra.AbsoluteConstraints(670, 170, 60, 40));
+
+        lblb10.setText("jLabel2");
+        getContentPane().add(lblb10, new org.netbeans.lib.awtextra.AbsoluteConstraints(740, 170, 60, 40));
+
+        lblc1.setText("jLabel2");
+        getContentPane().add(lblc1, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 230, 50, 40));
+
+        lblc2.setText("jLabel2");
+        getContentPane().add(lblc2, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 230, 60, 40));
+
+        lblc3.setText("jLabel2");
+        getContentPane().add(lblc3, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 230, 60, 40));
+
+        lblc4.setText("jLabel2");
+        getContentPane().add(lblc4, new org.netbeans.lib.awtextra.AbsoluteConstraints(320, 230, 60, 40));
+
+        lblc5.setText("jLabel2");
+        getContentPane().add(lblc5, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 230, 60, 40));
+
+        lbld1.setText("jLabel2");
+        getContentPane().add(lbld1, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 290, 50, 40));
+
+        lble1.setText("jLabel2");
+        getContentPane().add(lble1, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 350, 50, 40));
+
+        lbld2.setText("jLabel2");
+        getContentPane().add(lbld2, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 290, 60, 40));
+
+        lble2.setText("jLabel2");
+        getContentPane().add(lble2, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 350, 60, 40));
+
+        lbld3.setText("jLabel2");
+        getContentPane().add(lbld3, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 290, 60, 40));
+
+        lble3.setText("jLabel2");
+        getContentPane().add(lble3, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 350, 60, 40));
+
+        lbld4.setText("jLabel2");
+        getContentPane().add(lbld4, new org.netbeans.lib.awtextra.AbsoluteConstraints(320, 290, 60, 40));
+
+        lble4.setText("jLabel2");
+        getContentPane().add(lble4, new org.netbeans.lib.awtextra.AbsoluteConstraints(320, 350, 60, 40));
+
+        lbld5.setText("jLabel2");
+        getContentPane().add(lbld5, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 290, 60, 40));
+
+        lble5.setText("jLabel2");
+        getContentPane().add(lble5, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 350, 60, 40));
+
+        lblc6.setText("jLabel2");
+        getContentPane().add(lblc6, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 230, 60, 40));
+
+        lbld6.setText("jLabel2");
+        getContentPane().add(lbld6, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 290, 60, 40));
+
+        lble6.setText("jLabel2");
+        getContentPane().add(lble6, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 350, 60, 40));
+
+        lblc7.setText("jLabel2");
+        getContentPane().add(lblc7, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 230, 60, 40));
+
+        lbld7.setText("jLabel2");
+        getContentPane().add(lbld7, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 290, 60, 40));
+
+        lble7.setText("jLabel2");
+        getContentPane().add(lble7, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 350, 60, 40));
+
+        lblc8.setText("jLabel2");
+        getContentPane().add(lblc8, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 230, 60, 40));
+
+        lbld8.setText("jLabel2");
+        getContentPane().add(lbld8, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 290, 60, 40));
+
+        lble8.setText("jLabel2");
+        getContentPane().add(lble8, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 350, 60, 40));
+
+        lblc9.setText("jLabel2");
+        getContentPane().add(lblc9, new org.netbeans.lib.awtextra.AbsoluteConstraints(670, 230, 60, 40));
+
+        lbld9.setText("jLabel2");
+        getContentPane().add(lbld9, new org.netbeans.lib.awtextra.AbsoluteConstraints(670, 290, 60, 40));
+
+        lble9.setText("jLabel2");
+        getContentPane().add(lble9, new org.netbeans.lib.awtextra.AbsoluteConstraints(670, 350, 60, 40));
+
+        lblc10.setText("jLabel2");
+        getContentPane().add(lblc10, new org.netbeans.lib.awtextra.AbsoluteConstraints(740, 230, 60, 40));
+
+        lbld10.setText("jLabel2");
+        getContentPane().add(lbld10, new org.netbeans.lib.awtextra.AbsoluteConstraints(740, 290, 60, 40));
+
+        lble10.setText("jLabel2");
+        getContentPane().add(lble10, new org.netbeans.lib.awtextra.AbsoluteConstraints(740, 350, 60, 40));
+
+        lbla2.setText("jLabel2");
+        getContentPane().add(lbla2, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 120, 60, 30));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon("C:\\Users\\Fulvio\\source\\clases\\clasesST\\patrones\\Banco\\src\\img\\Transferencias.png")); // NOI18N
+        jLabel1.setText("jLabel1");
+        jLabel1.setMaximumSize(new java.awt.Dimension(860, 550));
+        jLabel1.setMinimumSize(new java.awt.Dimension(860, 550));
+        jLabel1.setPreferredSize(new java.awt.Dimension(858, 544));
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, 520));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -101,6 +279,56 @@ public class GUI_TarjetaTransferencias extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel lbl10;
+    private javax.swing.JLabel lbla1;
+    private javax.swing.JLabel lbla2;
+    private javax.swing.JLabel lbla3;
+    private javax.swing.JLabel lbla4;
+    private javax.swing.JLabel lbla5;
+    private javax.swing.JLabel lbla6;
+    private javax.swing.JLabel lbla7;
+    private javax.swing.JLabel lbla8;
+    private javax.swing.JLabel lbla9;
+    private javax.swing.JLabel lblb1;
+    private javax.swing.JLabel lblb10;
+    private javax.swing.JLabel lblb2;
+    private javax.swing.JLabel lblb3;
+    private javax.swing.JLabel lblb4;
+    private javax.swing.JLabel lblb5;
+    private javax.swing.JLabel lblb6;
+    private javax.swing.JLabel lblb7;
+    private javax.swing.JLabel lblb8;
+    private javax.swing.JLabel lblb9;
+    private javax.swing.JLabel lblc1;
+    private javax.swing.JLabel lblc10;
+    private javax.swing.JLabel lblc2;
+    private javax.swing.JLabel lblc3;
+    private javax.swing.JLabel lblc4;
+    private javax.swing.JLabel lblc5;
+    private javax.swing.JLabel lblc6;
+    private javax.swing.JLabel lblc7;
+    private javax.swing.JLabel lblc8;
+    private javax.swing.JLabel lblc9;
+    private javax.swing.JLabel lbld1;
+    private javax.swing.JLabel lbld10;
+    private javax.swing.JLabel lbld2;
+    private javax.swing.JLabel lbld3;
+    private javax.swing.JLabel lbld4;
+    private javax.swing.JLabel lbld5;
+    private javax.swing.JLabel lbld6;
+    private javax.swing.JLabel lbld7;
+    private javax.swing.JLabel lbld8;
+    private javax.swing.JLabel lbld9;
+    private javax.swing.JLabel lble1;
+    private javax.swing.JLabel lble10;
+    private javax.swing.JLabel lble2;
+    private javax.swing.JLabel lble3;
+    private javax.swing.JLabel lble4;
+    private javax.swing.JLabel lble5;
+    private javax.swing.JLabel lble6;
+    private javax.swing.JLabel lble7;
+    private javax.swing.JLabel lble8;
+    private javax.swing.JLabel lble9;
     // End of variables declaration//GEN-END:variables
 }
